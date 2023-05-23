e6d63841be48065965575ad07ec447b1ae0123f1
khannamanas04 <97518705+khannamanas04@users.noreply.github.com>
2023-05-23 09:28:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomNumber1 = (Math.floor(Math.random()*6))+1;
 
+document.querySelector(".img1").setAttribute("src","./images/dice"+randomNumber1+".png");
 
-var randomNumber2 = Math.floor(Math.random()*6)+1;
+var randomNumber2 = (Math.floor(Math.random()*6))+1;
 
+document.querySelector(".img2").setAttribute("src","./images/dice"+randomNumber2+".png");
 
-document.querySelector(".dice .img1").setAttribute("src", "dice"+randomNumber1+".png");
-
-
-document.querySelector(".dice .img2").setAttribute("src", "dice"+randomNumber2+".png");
-
-if(randomNumber1>randomNumber2){
-  document.querySelector(".container h1").innerHTML="🚩Player 1 Wins";
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").textContent="🚩Player 1 Wins!";
 }
-if(randomNumber2>randomNumber1){
-  document.querySelector(".container h1").textContent="Player 2 Wins🚩";
+else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").textContent="Player 2 Wins! 🚩";
 }
-if(randomNumber1===randomNumber2){
-  document.querySelector(".container h1").textContent="DRAW 🏳️";
+else{
+    document.querySelector("h1").textContent="Draw! 🏳️";
 }
