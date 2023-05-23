17676c01be24a891d47339bfbae3eaf26534aa9f
khannamanas04 <97518705+khannamanas04@users.noreply.github.com>
2023-05-23 09:37:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var randomNumber1 = (Math.floor(Math.random()*6))+1;
 
-document.querySelector(".img1").setAttribute("src","./images/dice"+randomNumber1+".png");
+document.querySelector(".img1").setAttribute("src","dice"+randomNumber1+".png");
 
 var randomNumber2 = (Math.floor(Math.random()*6))+1;
 
-document.querySelector(".img2").setAttribute("src","./images/dice"+randomNumber2+".png");
+document.querySelector(".img2").setAttribute("src","dice"+randomNumber2+".png");
 
 if(randomNumber1 > randomNumber2){
     document.querySelector("h1").textContent="🚩Player 1 Wins!";
